198e36c22798a3f9dd29b3f59cb724d3f74fcfb3
Ruben Gees <rubengees7@googlemail.com>
2018-07-11 22:37:13 +0200

    Prepare project with some predefined code and comments

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,44 +5,22 @@
     <div class="container mt-4">
       <div class="row justify-content-center">
         <b-list-group class="col-8 col-offset-2">
-          <b-list-group-item v-for="(note, i) in notes" :key="note.id" class="d-flex">
 
-            <b-form-input
-                v-if="note.isEditing"
-                v-model="note.content"
-                type="text"
-                class="align-self-center mr-auto"
-            >
-            </b-form-input>
+          <!--TODO: Iterate through notes.-->
+          <b-list-group-item class="d-flex">
 
-            <span
-                v-else
-                class="note-text align-self-center mr-auto"
-                :class="[note.isDone ? 'done-note' : '']"
-                @click="toggleDone(note, i)"
-            >
-              {{ note.content }}
+            <!--TODO: Set class based on done state.-->
+            <!--TODO: Click listener for toggling the done state.-->
+            <span class="note-text align-self-center mr-auto">
+              TODO: Show content of individual note in list
             </span>
-
-            <b-button
-                variant="primary"
-                class="align-self-center ml-4"
-                @click="toggleOrEditNote(note, i)"
-                :disabled="note.isLoading"
-            >
-              <i
-                  class="fa"
-                  :class="[note.isEditing ? 'fa-save' : 'fa-edit']"
-              >
-              </i>
-            </b-button>
-
           </b-list-group-item>
 
           <b-list-group-item class="d-flex">
-            <b-form-input v-model="newContent" type="text" class="mr-auto" placeholder="Neue Notiz..."></b-form-input>
+            <!--TODO: Save input for new note.-->
+            <b-form-input type="text" class="mr-auto" placeholder="Neue Notiz..."></b-form-input>
 
-            <b-button variant="primary" class="align-self-center ml-4" @click="createNote()" :disabled="newLoading">
+            <b-button variant="primary" class="align-self-center ml-4" @click="createNote()">
               <i class="fa fa-save"></i>
             </b-button>
           </b-list-group-item>
@@ -53,96 +31,29 @@
 </template>
 
 <script>
-/* eslint-disable no-console */
+/* eslint-disable */
 
-import Vue from "vue";
 import axios from "axios";
 
 export default {
   data() {
     return {
-      notes: [],
-      newContent: null,
-      newLoading: false
+      notes: []
+      // TODO: Property for text of new note input.
     };
   },
 
   mounted() {
-    axios
-      .get("http://localhost:8000/api/notes")
-      .then(response => {
-        this.notes = response.data.map(it =>
-          Object.assign({ isEditing: false, isLoading: false }, it)
-        );
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    // TODO: Retrieve notes and show.
   },
 
   methods: {
-    toggleDone(note, position) {
-      if (!note.isLoading) {
-        note.isLoading = true;
-
-        const copy = Object.assign({}, note, { isDone: !note.isDone });
-
-        this.editNote(copy, position);
-      }
-    },
-
-    toggleOrEditNote(note, position) {
-      if (note.isEditing) {
-        if (note.content && note.content.length > 0) {
-          note.isLoading = true;
-
-          this.editNote(note, position);
-        }
-      } else {
-        note.isEditing = true;
-      }
-    },
-
-    editNote(note, position) {
-      axios
-        .put("http://localhost:8000/api/notes/" + note.id, {
-          content: note.content,
-          isDone: note.isDone
-        })
-        .then(() => {
-          Vue.set(this.notes, position, note);
-
-          note.isEditing = false;
-          note.isLoading = false;
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    toggleDone(note) {
+      // TODO: Update note on the backend and show updated note in the list.
     },
 
     createNote() {
-      if (this.newContent && this.newContent.length > 0) {
-        this.newLoading = true;
-
-        axios
-          .post("http://localhost:8000/api/notes", {
-            content: this.newContent,
-            isDone: false
-          })
-          .then(response => {
-            return axios.get(
-              "http://localhost:8000" + response.headers.location
-            );
-          })
-          .then(response => {
-            this.notes.push(response.data);
-            this.newContent = null;
-            this.newLoading = false;
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
+      // TODO: Create note on the backend and add to the list.
     }
   }
 };
